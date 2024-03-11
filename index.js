fd6be5ce9601df241ba8b36a5ebfeaefa9f2cf0c
Theolewis1996 <theolewis1996@gmail.com>
2024-03-11 00:35:44 -0500

    stringify object

--- index.js
+++ index.js
@@ -1,3 +1,94 @@
+function createJSON() {
+  const person = {
+    name: 'TJ Lewis',
+    age: 28
+  };
+  return person;
+}
+
+function logPersonProperties(person) {
+  return person.name + ',' + person.age;
+}
+
+function modifyAge(person, newAge) {
+  person.age = newAge;
+  return person;
+}
+
+function addJob(person, job) {
+  person.job = job;
+  return person;
+}
+
+function deleteAge(person) {
+  delete person.age;
+  return person;
+}
+
+function addGreet(person) {
+  person.greet = function() {
+    return 'Hi, ' + this.name;
+  };
+  return person.greet();
+}
+
+function createNumbersArray() {
+  const numbers = [1, 7, 11, 23, 50];
+  return numbers;
+}
+
+function logFirstAndThird(numbers) {
+  return [numbers[0], numbers[2]];
+}
+
+function modifySecond(numbers, newNumber) {
+  numbers[1] = newNumber;
+  return numbers;
+}
+
+function logLength(numbers) {
+  return numbers.length;
+}
+
+function iterateWithFor(numbers) {
+  const output = [];
+  for (let i = 0; i < numbers.length; i++) {
+    output.push(numbers[i]);
+  }
+  return output;
+}
+
+function iterateWithWhile(numbers) {
+  const output = [];
+  let i = 0;
+  while (i < numbers.length) {
+    output.push(numbers[i]);
+    i++;
+  }
+  return output;
+}
+
+function iterateWithForOf(numbers) {
+  const output = [];
+  for (const number of numbers) {
+    output.push(number);
+  }
+  return output;
+}
+
+function createJSON(person) {
+  return JSON.stringify(person);
+}
+
+function parseJSON(json) {
+  return JSON.parse(json);
+}
+
+function stringifyPerson(person) {
+  return JSON.stringify(person);
+}
+
+
 // JavaScript Coding Challenge
 
 // 1. Create a function named 'createPerson' that returns an object named 'person' using object literal syntax. The object should have properties 'name' and 'age', with your name and age as the values.
@@ -7,7 +98,6 @@
 
 
 // 3. Create a function 'modifyAge' that takes a 'person' object and a new age as arguments, modifies the 'age' property of the 'person' object to the new age, and returns the updated object.
-
 
 // 4. Create a function 'addJob' that takes a 'person' object and a job as arguments, adds a new property 'job' to the 'person' object with the job as its value, and returns the updated object.
 
@@ -50,110 +140,7 @@
 
 // Do not alter below this line please... Doing so will BREAK automatic grading for your assignment!
 // 1.
-function createPerson() {
-  const person = {
-    name: 'Ben Feria',
-    age: 27
-  };
-  return person;
-}
 
-// 2.
-function logPersonProperties(person) {
-  return person.name + ', ' + person.age;
-}
-
-// 3.
-function modifyAge(person, newAge) {
-  person.age = newAge;
-  return person;
-}
-
-// 4.
-function addJob(person, job) {
-  person.job = job;
-  return person;
-}
-
-// 5.
-function deleteAge(person) {
-  delete person.age;
-  return person;
-}
-
-// 6.
-function addGreet(person) {
-  person.greet = function() {
-    return 'Hi, ' + this.name;
-  };
-  return person.greet();
-}
-
-// 7.
-function createNumbersArray() {
-  const numbers = [11, 23, 1, 9, 95];
-  return numbers;
-}
-
-// 8.
-function logFirstAndThird(numbers) {
-  return [numbers[0], numbers[2]];
-}
-
-// 9.
-function modifySecond(numbers, newNumber) {
-  numbers[1] = newNumber;
-  return numbers;
-}
-
-// 10.
-function logLength(numbers) {
-  return numbers.length;
-}
-
-// 11.
-function iterateWithFor(numbers) {
-  const output = [];
-  for (let i = 0; i < numbers.length; i++) {
-    output.push(numbers[i]);
-  }
-  return output;
-}
-
-// 12.
-function iterateWithWhile(numbers) {
-  const output = [];
-  let i = 0;
-  while (i < numbers.length) {
-    output.push(numbers[i]);
-    i++;
-  }
-  return output;
-}
-
-// 13.
-function iterateWithForOf(numbers) {
-  const output = [];
-  for (const number of numbers) {
-    output.push(number);
-  }
-  return output;
-}
-
-// 14.
-function createJSON(person) {
-  return JSON.stringify(person);
-}
-
-// 15.
-function parseJSON(json) {
-  return JSON.parse(json);
-}
-
-// 16.
-function stringifyPerson(person) {
-  return JSON.stringify(person);
-}
 
 export {
   createPerson,
